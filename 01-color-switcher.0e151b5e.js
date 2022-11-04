f95f0d3c2f660a693fa1a60bdba53b983552e8e3
TetianaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-11-04 17:10:19 +0000

    Deploying to gh-pages from @ TetianaVielkova/goit-js-hw-09@04bac020ee03a36970f1d6e7a925c1bae5819acc 🚀

--- 01-color-switcher.0e151b5e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){false;e=setInterval((function(){console.log(o()),document.body.style.backgroundColor=o()}),1e3)})),n.addEventListener("click",(function(){clearInterval(e)}));var e=null;function o(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}}();
-//# sourceMappingURL=01-color-switcher.0e151b5e.js.map
---
+++ 01-color-switcher.ad07a43b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval((()=>{console.log(o()),document.body.style.backgroundColor=o()}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!1}));let n=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}
+//# sourceMappingURL=01-color-switcher.ad07a43b.js.map
