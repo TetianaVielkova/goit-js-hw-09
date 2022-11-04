98bcc1ebb8c7e8e234ea20ba00073ed56dcf742c
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-11-04 13:06:16 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', changeBGColor)
+stopBtn.addEventListener('click', intervalDelete)
+
+let intervalID = null;
+const isActive = false;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBGColor() {
+    if(isActive){
+        return;
+    }
+    intervalID = setInterval(()=>{
+        console.log(getRandomHexColor())
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+
+function intervalDelete() {
+    clearInterval(intervalID);
+}
+
